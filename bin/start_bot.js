4f8f93204d6726224d913e2f39b8d40c2baf2061
Junior José <contato@autopyweb.com>
2020-12-06 19:14:09 -0300

    teste

--- bin/start_bot.js
+++ bin/start_bot.js
@@ -1,7 +1,21 @@
-const venom = require('venom-bot');
+const venom = require('@open-wa/wa-automate');
 const start = require('./whatsapp')
 
-var teste = venom.create('Auto Py Web2')
+const launchConfig ={
+    sessionId: "Auto Py Web",
+    authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+    blockCrashLogs: true,
+    disableSpins: true,
+    headless: true,
+    hostNotificationLang: 'PT_BR',
+    useChrome: true,
+    logConsole: false,
+    popup: true,
+    qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+
+  }
+
+var teste = venom.create(launchConfig)
 
 
 module.exports = teste
--- bin/whatsapp.js
+++ bin/whatsapp.js
@@ -3,6 +3,8 @@ const uuid = require('uuid');
 const bd = require("../models/base.db");
 const fs = require('fs');
 const mime = require('mime-types');
+const { log } = require('console');
+const { measureMemory } = require('vm');
 
 
 class windows {
@@ -13,41 +15,89 @@ class windows {
     }
 
 
+
     // Inicia Conversa Bot
-    async start(client = this.client, io = this.io) {
+    start(client = this.client, io = this.io) {
         let menssagem = []
-        let send = [{from: '551236005005@c.us'}]
-        
-       
-        //const allMessages = await client.getAllMessagesInChat('551236005005@c.us');
-       
+        let ackT = [{ t: '1607083688' },]
+
+
+
+        function decodeBase64Image(dataString) {
+            var matches = dataString.match(/^data:([A-Za-z-+0-9\/]+);base64,(.+)$/),
+                response = {};
+
+            if (matches.length !== 3) {
+                return new Error('Invalid input string');
+            }
+
+
+            response.type = matches[1];
+            response.data = new Buffer.from(matches[2], 'base64');
+
+            return response;
+        }
+        function sleep(time) {
+            return new Promise((resolve) => setTimeout(resolve, time));
+        }
+
         io.on('connection', async function (socket) {
             const chats = await client.getAllChats();
-            io.emit('listContact', chats)
-            socket.emit('previusMenssager', menssagem)
+            socket.emit('listContact', chats)
+            socket.on('room', async function (room) {
+                socket.join(room);
+                const chat22 = await client.getAllMessagesInChat(`${room}@c.us`, true)
+                io.to(room).emit('chat', chat22)
+                console.log(chat22.id)
+            });
+            socket.on('leave-room', function (room) {
+                socket.leave(room, function (err) {
+                    console.log(err); // display null
+                    console.log(socket.adapter.rooms);  // display the same list of rooms the specified room is still there
+                });
+                console.log(`${socket.id} saiu da sala ${room}`)
+                console.log(socket.rooms);
+            });
             socket.on('sendMenssage', (data) => {
                 menssagem.push(data)
-                
-                var name_wpp = function (data) {        
-                    if(data.from !== undefined){
-                        send.push({from: data.from})
-                        return data.from
-                    }else{
-                        return '551236005005@c.us'
-                    }
-                }
-                name_wpp(data)
-
-                socket.broadcast.emit('receivedMensagem', data);
                 client
-                    .sendText(send[send.length - 1].from, data.resposta)
+                    .sendText(data.from, data.resposta)
             })
 
         });
 
+        client.onAck((ack) => {
+            async function sendSocket(ack) {
+                io.to(ack.to.match(/([0-9]+)/)[0]).emit('updateWhatsapp', ack)
+                menssagem.push(ack)
+                const chats = await client.getAllChats();
+                io.emit('listContact', chats)
+            }
 
-        client.onMessage((message) => {
+            if (ack.ack === 1 && ackT[ackT.length - 1].t !== ack.t) {
+                sendSocket(ack)
+                ackT.push({ t: ack.t })
+            }
+        });
 
+        client.onAnyMessage((message)=>{
+            const tel = message.to.match(/([0-9]+)/)
+            console.log(message);
+
+            //client.sendLocation('5512982062736@c.us', '-23.7991625', '-45.3587645')
+            if(message.to === '551236005005@c.us')
+                io.in(message.from.match(/([0-9]+)/)[0]).emit('updateWhatsapp', message)
+            else
+                io.in(message.to.match(/([0-9]+)/)[0]).emit('updateWhatsapp', message)
+
+        })
+
+
+        client.onMessage(async (message) => {
+            //console.log(message)
+            const sessionId = uuid.v4();
+            const projectId = 'agente-vendas-lnxa'
+            const fileNameNow = uuid.v4();
             const name_wpp = function () {
                 if (message.sender.pushname !== undefined) {
                     return message.sender.pushname
@@ -55,25 +105,133 @@ class windows {
                     return message.sender.verifiedName
                 }
             };
-            var mm = {
-                mensagem: message.body,
-                resposta: '',
-                imagem: message.sender.profilePicThumbObj.eurl,
-                from: message.sender.id,
-                name: name_wpp(),
 
+            await bd.whatsapp.create({
+                sessionid: sessionId,
+                chatid: message.chatId,
+                name_save: `${message.sender.formattedName}`,
+                name_wpp: name_wpp()
+            })
+                .then((certo) => console.log('Criado com Sucesso!'))
+                .catch((erro) => console.error('Já exite um contato com essa ChatId: ', message.chatId));
+    
+            //Busca o chatId para inicia a conversa.
+    
+            const users = await bd.whatsapp.findOne({
+                where: {
+                    chatid: message.chatId
+                }
+            });
+    
+            //Key para  ser usado na conversa com o Google
+            const userKey = users.sessionid
+    
+            //KeyFile da API GOOGLE
+            const sessionClient = new dialogflow.SessionsClient({
+                keyFilename: "./agente-vendas-lnxa-b897141276bc.json"
+            });
+    
+            //
+            const sessionPath = sessionClient.projectAgentSessionPath(projectId, userKey);
+            const request = {
+                session: sessionPath,
+                queryInput: {
+                    text: {
+                        // The query to send to the dialogflow agent
+                        text: message.body,
+                        // The language used by the client (en-US)
+                        languageCode: 'pt-BR',
+                    },
+                },
+    
+            };
+    
+            //Resposta do Diagflow
+            const responses = await sessionClient.detectIntent(request);
+            const result = responses[0].queryResult;
+    
+            //Criação do LOG de Conversa na database
+            await bd.Chat.create({
+                mensagem: message.body,
+                resposta: result.fulfillmentText,
+                chatId: users.id,
+                whatsappId: users.id
+            })
+
+
+            //sconsole.log(message.body)
+
+            if (message.type === "location") {
+                if (message.shareDuration) {
+
+                    //client.onLiveLocation(message.from, location => console.log(location))
+
+                    const forcedLiveLocation = await client.forceUpdateLiveLocation(message.from);
+
+                    //const motoristaNumber = forcedLiveLocation[0].id.replace('@c.us', '');
+                    const motoristaLatitude = forcedLiveLocation[0].lat;
+                    const motoristaLongitude = forcedLiveLocation[0].lng;
+                    const motoristaData = forcedLiveLocation[0].lastUpdated;
+                    // Loop to update informations
+                    const time = setInterval(async function () {
+
+                        console.log('Latitude: ' + motoristaLatitude,
+                            'Longitude: ' + motoristaLongitude,
+                            'Last Update: ' + motoristaData);
+                        await client
+                            .sendLocation('5512982062736@c.us', '-23.7991625', '-45.3587645')
+
+
+                    }, 10000);
+                    //console.log(60*message.shareDuration)
+                    await sleep(1000 * message.shareDuration)
+                    //console.log('Parou')
+                    clearInterval(time)
+                }
             }
-            io.emit('previusMenssager2', mm)
-            menssagem.push(mm)
-        })
+
+            //console.log(message);
+
+            if (message.mimetype) {
+                //console.log(message.mimetype)
+
+                const buffer = await client.decryptMedia(message);
+                //console.log(buffer)
+
+                const dataBuffer = decodeBase64Image(buffer)
+
+                const dir = `media/${message.chatId}/`
+                if (!fs.existsSync(dir)) {
+                    //Efetua a criação do diretório
+                    fs.mkdirSync(dir);
+                }
+                //Criação da Media e Pasta
+                const fileName = `${dir}${fileNameNow}.${mime.extension(message.mimetype)}`;
+                await fs.writeFile(fileName, dataBuffer.data, (err) => {
+                });
+                //console.log('aqui')
+                client.sendFile(message.from, buffer, fileName, 'Teste')
+            }
+
+            if (message.body === "!Stop" && message.isGroupMsg === false) {
+                //console.log('Aqui para')
+                //clearInterval(time)
+            }
+            //client.sendLocation('5512982062736@c.us', '-23.7991625', '-45.3587645')
+            const chats = await client.getAllChats();
+            io.emit('listContact', chats)
+            client.sendSeen(message.from);
+
+        }
+        )
 
         /**
         client.onMessage(async (message) => {
             const fileNameNow = uuid.v4();
             const sessionId = uuid.v4();
             const projectId = 'agente-vendas-lnxa'
-
-
+    
+    
             //Download de Media enviadas do Wpp
             if (message.isMedia === true || message.isMMS === true) {
                 const buffer = await client.decryptFile(message);
@@ -87,7 +245,7 @@ class windows {
                 await fs.writeFile(fileName, buffer, (err) => {
                 });
             }
-
+    
             //Verifica o nome do WPP (Sendo empresa ou Usuario Comum)
             const name_wpp = function () {
                 if (message.sender.pushname !== undefined) {
@@ -96,7 +254,7 @@ class windows {
                     return message.sender.verifiedName
                 }
             };
-
+    
             //Criação do Banco de Dados WhatsApp
             await bd.whatsapp.create({
                 sessionid: sessionId,
@@ -106,23 +264,23 @@ class windows {
             })
                 .then((certo) => console.log('Criado com Sucesso!'))
                 .catch((erro) => console.error('Já exite um contato com essa ChatId: ', message.chatId));
-
+    
             //Busca o chatId para inicia a conversa.
-
+    
             const users = await bd.whatsapp.findOne({
                 where: {
                     chatid: message.chatId
                 }
             });
-
+    
             //Key para  ser usado na conversa com o Google
             const userKey = users.sessionid
-
+    
             //KeyFile da API GOOGLE
             const sessionClient = new dialogflow.SessionsClient({
                 keyFilename: "./agente-vendas-lnxa-b897141276bc.json"
             });
-
+    
             //
             const sessionPath = sessionClient.projectAgentSessionPath(projectId, userKey);
             const request = {
@@ -135,13 +293,13 @@ class windows {
                         languageCode: 'pt-BR',
                     },
                 },
-
+    
             };
-
+    
             //Resposta do Diagflow
             const responses = await sessionClient.detectIntent(request);
             const result = responses[0].queryResult;
-
+    
             //Criação do LOG de Conversa na database
             await bd.Chat.create({
                 mensagem: message.body,
@@ -149,12 +307,12 @@ class windows {
                 chatId: users.id,
                 whatsappId: users.id
             })
-
-
-
+    
+    
+    
             //Envio da Mensagem para o Clinte do WPP
-
-
+    
+    
             let menssagem = []
             io.on('connection', function (socket) {
                 socket.emit('previusMenssager', menssagem)
@@ -163,12 +321,12 @@ class windows {
                     socket.broadcast.emit('receivedMensagem', data);
                     client.sendText(message.from, data.resposta)
                 })
-    
+     
             });
-
+    
         
-
-
+    
+    
             var mm = {
                 mensagem: message.body,
                 resposta: '',
@@ -177,10 +335,10 @@ class windows {
             io.emit('previusMenssager2', mm)
             io.broadcast.emit('previusMenssager2', mm)
             menssagem.push(menssagem)
-
-
-
-
+    
+    
+    
+    
         })
          */
 
