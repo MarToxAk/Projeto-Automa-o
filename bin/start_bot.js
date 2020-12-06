60466068d0862e45b8b91bf6eb23e77b53fcb5d5
Junior José <contato@autopyweb.com>
2020-12-06 20:37:31 -0300

    teste

--- bin/start_bot.js
+++ bin/start_bot.js
@@ -6,7 +6,7 @@ const launchConfig ={
     authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
     blockCrashLogs: true,
     disableSpins: true,
-    headless: false,
+    headless: true,
     hostNotificationLang: 'PT_BR',
     useChrome: true,
     logConsole: false,
